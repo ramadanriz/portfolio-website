227a199003ea7411d2481bc9084936d1c45108c3
ramadanriz <zerorider96@gmail.com>
2023-07-24 17:02:11 +0700

    add loading feature

---
+++ src/components/Loading.jsx
@@ -0,0 +1,8 @@
+export const Loading = () => {
+  return (
+    <div className='min-h-[65vh] md:min-h-[80vh] flex justify-center items-center flex-col'>
+        <span className="loading loading-dots loading-md"></span>
+        <p className='text-xl'>Loading...</p>
+    </div>
+  )
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,24 +3,31 @@ import axios from 'axios'
 import { baseUrl } from '../constant'
 import SectionTitle from "../components/SectionTitle"
 import List from '../components/projects/List'
+import { Loading } from '../components/Loading'
 
 const Projects = () => {
   const [project, setProject] = useState()
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     axios.get(baseUrl).then((response) => {
       setProject(response.data)
+      setIsLoading(false)
     })
   }, [])
 
-  if (!project) return null
-
   return (
     <div className='mt-2'>
-      <SectionTitle>Projects</SectionTitle>
-      <p>List of my top featured projects repositories. 🔖</p>
-      <p>Feel free to pay a visit and don&apost forget to leave me a star! ⭐</p>
-      <List projects={project} /> 
+      {isLoading ? ( 
+        <Loading />
+      ) : (
+        <>
+          <SectionTitle>Projects</SectionTitle>
+          <p>List of my top featured projects repositories. 🔖</p>
+          <p>Feel free to pay a visit and don&apost forget to leave me a star! ⭐</p>
+          <List projects={project} />
+        </>
+      )}
     </div>
   )
 }
