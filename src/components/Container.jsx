97190a702824d1d5d75c97734f563bcc79a2c20e
ramadanriz <zerorider96@gmail.com>
2023-11-01 16:15:05 +0700

    add footer

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,19 +1,19 @@
-import PropTypes from 'prop-types'
-import Navbar from "./Navbar"
+import PropTypes from "prop-types";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Container = (props) => {
   return (
     <>
-        <Navbar />
-        <main className='relative mx-auto mt-16 md:mt-14 py-6 px-8 md:px-16 lg:px-32 xl:px-72 lg:container'>
-          {props.children}
-        </main>
+      <Navbar />
+      <main className="relative mx-auto mt-16 md:mt-14 py-6 px-8 md:px-16 lg:px-32 xl:px-72 lg:container">{props.children}</main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 Container.propTypes = {
-  children: PropTypes.node
-}
+  children: PropTypes.node,
+};
 
-export default Container
+export default Container;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import { GrLinkedin } from "react-icons/gr";
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
+      <aside className="mx-auto items-center grid-flow-col">
+        <p>© 2023 Rizky Ramadhani - Portfolio</p>
+      </aside>
+      <nav className="mx-auto md:place-self-center md:justify-self-end">
+        <div className="grid grid-flow-col gap-4">
+          <a href="https://www.linkedin.com/in/ramadanriz/" target="_blank" rel="noreferrer">
+            <GrLinkedin className="text-2xl" />
+          </a>
+          <a href="https://github.com/ramadanriz" target="_blank" rel="noreferrer">
+            <FaGithub className="text-2xl" />
+          </a>
+        </div>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
